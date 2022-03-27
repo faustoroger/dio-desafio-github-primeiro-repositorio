807b01eabfe9640cbcb648aad0c12823a9d1a6fc
Fausto Roger <hixian@gmail.com>
2022-03-27 20:24:06 -0300

    adiciona aula03

---
+++ Python/01.Introdução a programação com Python/aula_03-2.py
@@ -0,0 +1,9 @@
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+
+resto_a = a % 2
+resto_b = b % 2
+if resto_a == 0 or not resto_b > 0:
+    print('Foi digitado um número par')
+else:    
+    print('Nenhum numero par foi digitado')
