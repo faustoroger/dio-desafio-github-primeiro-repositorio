2a26a53e982473b892a42d7febd9fc6db0d29527
Fausto Roger <hixian@gmail.com>
2022-03-31 12:52:26 -0300

    adiciona aula08

---
+++ Python/01.Introdução a programação com Python/aula_08_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# contador_letras
+
+def contador_letras(lista_palavras: list):
+    contador = []
+    for x in lista_palavras:
+        quantidade = len(x)
+        contador.append(quantidade)
+
+    return contador
+
+
+def teste():
+    return 'teste'
+
+
+if __name__ == "__main__":
+    lista = ['cachorro', 'gato']
+    print(contador_letras(lista))
