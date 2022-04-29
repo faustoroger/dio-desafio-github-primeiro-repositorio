e88d5a8c3436527156af798025c38d71ffdc53e4
Fausto Roger <hixian@gmail.com>
2022-04-29 00:33:01 -0300

    altera o nome do package

--- Python/Desafios/03-Desafios.Descomplicando.a.Criação.de.Pacotes/libtesthello/setup.py
+++ Python/Desafios/03-Desafios.Descomplicando.a.Criação.de.Pacotes/libtesthello/setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="libtesthello_fr",
+    name="libtesthello",
     version="0.0.2",
     author="fr",
     author_email='testsayhellofr@gmail.com',
     description='Biblioteca que diz Hello',
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/faustoroger/dio-desafio-github-primeiro-repositorio/tree/develop/Python/Desafios/03-Desafios.Descomplicando.a.Cria%C3%A7%C3%A3o.de.Pacotes/libtesthello-fr",
+    url="https://github.com/faustoroger/dio-desafio-github-primeiro-repositorio/tree/develop/Python/Desafios/03-Desafios.Descomplicando.a.Cria%C3%A7%C3%A3o.de.Pacotes/libtesthello",
     packages=find_packages(),
     install_requires=requirements,
     license='MIT'
